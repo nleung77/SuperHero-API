5889a6fa64014d031e954e417110aacd19414c3d
nleung77 <nleung77@gmail.com>
2023-04-24 14:52:48 -0400

    heroku

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
+const MONGODB_URI =
+  process.env.MONGO_URL || "mongodb://localhost:27017/superheroes";
+
+const mongooseConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 function connect() {
   mongoose
-    .connect("mongodb://localhost:27017/superheroes", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(MONGODB_URI, mongooseConfig)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 }
