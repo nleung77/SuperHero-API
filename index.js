76675f1c1acf36c1c2c5f73095e2e27be85676b9
nleung77 <nleung77@gmail.com>
2023-04-25 17:02:51 -0400

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./db/connection.js");
+const cors = require("cors");
 
 const heroesRouter = require("./routes/heroes");
 const villainsRouter = require("./routes/villains");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/heroes", heroesRouter);
 app.use("/api/villains", villainsRouter);
@@ -16,4 +18,3 @@ app.use("/api/villains", villainsRouter);
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
