f87f5e43f11e800873dd1c9634ff99b1a8e9949f
nleung77 <nleung77@gmail.com>
2023-04-25 12:34:06 -0400

    updatedschema

--- models/hero.js
+++ models/hero.js
@@ -1,28 +1,51 @@
 const mongoose = require("mongoose");
 
 const heroSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  intelligence: Number,
-  strength: Number,
-  speed: Number,
-  durability: Number,
-  power: Number,
-  combat: Number,
-  gender: String,
-  race: String,
-  height: {
-    feet: Number,
-    inches: Number,
+  name: String,
+  powerstats: {
+    intelligence: Number,
+    strength: Number,
+    speed: Number,
+    durability: Number,
+    power: Number,
+    combat: Number,
   },
-  weight: {
-    pounds: Number,
+  biography: {
+    full_name: String,
+    alter_egos: String,
+    aliases: [String],
+    place_of_birth: String,
+    first_appearance: String,
+    publisher: String,
+    alignment: String,
   },
-  alignment: String,
-  publisher: String,
-  imageUrl: String,
+  appearance: {
+    gender: String,
+    race: String,
+    height: {
+      ft: String,
+      cm: String,
+    },
+    weight: {
+      lb: String,
+      kg: String,
+    },
+    eye_color: String,
+    hair_color: String,
+  },
+  work: {
+    occupation: String,
+    base: String,
+  },
+  connections: {
+    group_affiliation: String,
+    relatives: String,
+  },
+  image: {
+    url: String,
+  },
 });
 
 const Hero = mongoose.model("Hero", heroSchema);
 
 module.exports = Hero;
-
